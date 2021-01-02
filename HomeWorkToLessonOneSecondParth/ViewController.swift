41b40f128def71f493cbbabd00bfeeab5d9fc7a8
Vladimir <kynicin87@mail.ru>
2021-01-03 01:36:56 +0200

    Initial Commit

---
+++ HomeWorkToLessonOneSecondParth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWorkToLessonOneSecondParth
+//
+//  Created by vladimir gennadievich on 03.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

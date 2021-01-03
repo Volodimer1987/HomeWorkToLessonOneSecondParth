c3383f887bd5e62d0af7f64c6f8fe93feb5f114f
Vladimir <kynicin87@mail.ru>
2021-01-03 13:03:46 +0200

    Code and button and light label is connected) Constrains too

--- HomeWorkToLessonOneSecondParth/ViewController.swift
+++ HomeWorkToLessonOneSecondParth/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var grennLightLabel: UIImageView!
+    @IBOutlet var yellowLightLabel: UIImageView!
+    @IBOutlet var redLightLabel: UIImageView!
+    
+    @IBOutlet var buttonLabel: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonLightAction() {
+    }
+    
 }
 

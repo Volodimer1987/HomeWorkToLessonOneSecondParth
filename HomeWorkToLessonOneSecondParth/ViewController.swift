dcb19d234a8a6df79dc34a0bb0ab434f95f674e8
Vladimir <kynicin87@mail.ru>
2021-01-04 14:02:18 +0200

    fnito)All is ready

--- HomeWorkToLessonOneSecondParth/ViewController.swift
+++ HomeWorkToLessonOneSecondParth/ViewController.swift
@@ -7,6 +7,13 @@
 
 import UIKit
 
+enum TrafficLightsColors {
+    case red
+    case yellow
+    case green
+}
+
+
 class ViewController: UIViewController {
 
     @IBOutlet var greenLightLabel: UIImageView!
@@ -14,6 +21,10 @@ class ViewController: UIViewController {
     @IBOutlet var redLightLabel: UIImageView!
     
     @IBOutlet var buttonLabel: UIButton!
+    
+    private var trafficLightColorNow:TrafficLightsColors = .green
+    private let lightIsOn:CGFloat = 1
+    private let lightIsOff:CGFloat = 0.3
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,9 +39,32 @@ class ViewController: UIViewController {
         yellowLightLabel.layer.cornerRadius = 50
         redLightLabel.layer.cornerRadius = 50
         
+        redLightLabel.alpha = lightIsOff
+        yellowLightLabel.alpha = lightIsOff
+        greenLightLabel.alpha = lightIsOff
+        
     }
 
     @IBAction func buttonLightAction() {
+        buttonLabel.setTitle("Next", for: .normal)
+        
+        switch trafficLightColorNow {
+        case .red:
+            trafficLightColorNow = .yellow
+            redLightLabel.alpha = lightIsOff
+            yellowLightLabel.alpha = lightIsOn
+            
+        case .yellow:
+            trafficLightColorNow = .green
+            yellowLightLabel.alpha = lightIsOff
+            greenLightLabel.alpha = lightIsOn
+            
+        case .green:
+            trafficLightColorNow = .red
+            greenLightLabel.alpha = lightIsOff
+            redLightLabel.alpha = lightIsOn
+
+        }
         
     }
     
